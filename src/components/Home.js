d22bd21a6ee0ab74d5cc7f15fd26a9defa3a0b37
Jeph Johnson <hello@jephjohnson.com>
2019-01-21 21:01:44 -0800

    Homepage Preview Buildout

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,78 +7,27 @@ const HomePageTemplate = ({
   fullImage,
   main
 }) => (
-  <section className="section section--gradient">
-    <div className="container">
-      <div className="section">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="content">
-			  <div
-                className="full-width-image-container margin-top-0"
-                style={{ backgroundImage: `url(${fullImage})` }}
-              >
-                <h2
-                  className="has-text-weight-bold is-size-1"
-                  style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
-                    color: 'white',
-                    padding: '1rem',
-                  }}
-                >
-                  {heading}
-                </h2>
-              </div>
-              <div className="columns">
-                <div className="column is-7">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    {heading}
-                  </h3>
-                  <p>{description}</p>
-                </div>
-              </div>
-              <div className="columns">
-                <div className="column is-7"></div>
-              </div>
-              <div className="tile is-ancestor">
-                <div className="tile is-vertical">
-                  <div className="tile">
-                    <div className="tile is-parent is-vertical">
-                      <article className="tile is-child">
-                        <img
-                          style={{ borderRadius: '5px' }}
-                          src={main.image1.image}
-                          alt={main.image1.alt}
-                        />
-                      </article>
+    <section className="hero" key={post.id} style={{ backgroundImage: `url(${fullImage})` }}>
+        <div className="container">
+            <div className="hero-body">
+                <div className="columns is-centered">
+                    <div className="column is-10-tablet is-9-desktop">
+                    <div className="columns intro">
+                        <div className="column">
+                            <h1>{heading}</h1>
+                        </div>
+                        <div className="column is-2 has-text-centered arrow-wrapper">
+                            <span className="arrow"></span>
+                        </div> 
+                        <div className="column hero-copy">
+                            <p>{description}</p>
+                        </div>
                     </div>
-                    <div className="tile is-parent">
-                      <article className="tile is-child">
-                        <img
-                          style={{ borderRadius: '5px' }}
-                          src={main.image2.image}
-                          alt={main.image2.alt}
-                        />
-                      </article>
                     </div>
-                  </div>
-                  <div className="tile is-parent">
-                    <article className="tile is-child">
-                      <img
-                        style={{ borderRadius: '5px' }}
-                        src={main.image3.image}
-                        alt={main.image3.alt}
-                      />
-                    </article>
-                  </div>
                 </div>
-              </div>
-          </div>
+            </div>
         </div>
-      </div>
-    </div>
-	</div>
-  </section>
+    </section>
 )
 
 HomePageTemplate.propTypes = {
