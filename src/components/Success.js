9ca9a8ad02a3c1197655eee5150c85d5fa16ed5f
Jeph Johnson <hello@jephjohnson.com>
2019-02-11 22:43:39 -0500

    more buildout

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,16 +1,8 @@
-import React from 'react'
+import React from "react";
 
-const Success = (props) => (
-    <div id="main" className="alt">
-        <section id="one">
-            <div className="inner">
-                <header className="major">
-                    <h1>Success/Thank You Page</h1>
-                </header>
-                <p>Thank you for contacting us!</p>
-            </div>
-        </section>
-    </div>
-)
-
-export default Success
+export default () => (
+  <div>
+    <h1>Thank you!</h1>
+    <p>This is a custom thank you page for form submissions</p>
+  </div>
+);
