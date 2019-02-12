b17430cdf7360e81ed36a6b1ef6997f70e0ce033
Jeph Johnson <hello@jephjohnson.com>
2019-02-11 22:51:42 -0500

    more buildout

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,84 +1,92 @@
-import React from "react";
-import { navigateTo } from "gatsby-link";
 
-function encode(data) {
-  const formData = new FormData();
+import React from 'react'
+import Success from './Success'
 
-  for (const key of Object.keys(data)) {
-    formData.append(key, data[key]);
-  }
-
-  return formData;
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
 }
 
 export default class ContactForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { firstname: "", lastname: "", email: "", phone: "", role: "", resume: "", isTrue: true };
   }
-
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
 
   handleAttachment = e => {
     this.setState({ [e.target.name]: e.target.files[0] });
   };
 
   handleSubmit = e => {
-    e.preventDefault();
-    const form = e.target;
     fetch("/", {
       method: "POST",
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...this.state
-      })
+      //headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
     })
-      .then(() => navigateTo(form.getAttribute("action")))
+      .then(() => /*window.location = '/admin/'*/this.setState({ firstname: "", lastname: "", email: "",  phone: "", role: "", resume: "", isTrue: false }))
       .catch(error => alert(error));
+
+    e.preventDefault();
   };
 
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
+    const { firstname, lastname, email, phone, role, resume, isTrue } = this.state;
     return (
-      <div>
-        <h1>File Upload</h1>
-        <form
-          name="file-upload"
-          method="post"
-          action="/Success/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={this.handleSubmit}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="file-upload" />
-          <p hidden>
-            <label>
-              Don’t fill this out:{" "}
-              <input name="bot-field" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your name:<br />
-              <input type="text" name="name" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              File:<br />
-              <input
+      <div className="careers">
+      <h3 className="has-text-centered">JOIN US</h3>
+      <form name="contact" onSubmit={this.handleSubmit} data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" />
+        <div className="field">
+          <div className="control">
+            <input className="input" type="text" placeholder="First Name" name="firstname" value={firstname} onChange={this.handleChange} />
+          </div>
+        </div>
+        <div className="field">
+          <div className="control">
+            <input className="input" type="text" placeholder="Last Name" name="lastname" value={lastname} onChange={this.handleChange} />
+          </div>
+        </div>
+        <div className="field">
+          <div className="control">
+            <input className="input" type="email" placeholder="Email" name="email" value={email} onChange={this.handleChange} />
+          </div>
+        </div>
+        <div className="field">
+          <div className="control">
+            <input className="input" type="tel" placeholder="Phone" name="phone" value={phone} onChange={this.handleChange} />
+          </div>
+        </div>
+        <div className="field">
+          <div className="control">
+            <input className="input" type="text" placeholder="Role you are seeking" name="role" value={role} onChange={this.handleChange} />
+          </div>
+        </div>
+        <div className="field">
+          <div className="control">
+            {/*<label className="input" htmlFor="upload-photo">Attach a resume</label>
+            <input className="input" type="file" id="upload-photo" accept="application/pdf, application/msword" name="resume" value={resume} onChange={this.handleChange} />
+          </div>*/}
+          <input
                 type="file"
                 name="attachment"
                 onChange={this.handleAttachment}
               />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+        </div>
+        <div className="field">
+          <div className="control has-text-centered">
+            <button className="button outline" type="submit" disabled={!email || !firstname || !lastname}>Send it.</button>
+          </div>
+        </div>
+      </form>
+      
+      {!isTrue ? (
+          <Success />
+        ) : (
+          null
+        )}
       </div>
     );
   }
