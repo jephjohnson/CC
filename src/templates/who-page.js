0207cdd7384f36bbab0ec8ffa41c882417ccee6e
Jeph Johnson <hello@jephjohnson.com>
2019-02-14 16:42:53 -0500

    more buildout

--- src/templates/who-page.js
+++ src/templates/who-page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import WhoPageTemplate from '../components/who'
+import WhoPageTemplate from '../components/Who'
 import { HTMLContent } from '../components/Content'
 
 const WhoPage = ({ data }) => {
